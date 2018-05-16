50a7926d8d81f53aa759baf75ef283b52620dd86
LeishaBurford <lburf102@uottawa.ca>
2018-05-16 21:11:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,10 @@ class TicTacToe
   def move(board, index, token)
     @board[index] = token
   end
+  
+  def position_taken?(board, index)
+    !(board[index].nil? || board[index] == " ")
+  end
 end
 
   
